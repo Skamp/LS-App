46f70e9a6974b5dab9935f68af55ea0601572c29
Skamp <sergio.gd@gmail.com>
2011-10-25 22:23:46 +0200

    Login with SharedPreferences

--- src/com/lsn/LoadSensing/LSLoginActivity.java
+++ src/com/lsn/LoadSensing/LSLoginActivity.java
@@ -2,7 +2,9 @@ package com.lsn.LoadSensing;
 import com.lsn.LoadSensing.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,6 +17,7 @@ public class LSLoginActivity extends Activity {
 
 	private String strUser;
 	private String strPass;
+	private SharedPreferences prefs;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,20 @@ public class LSLoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 		
+		prefs = getSharedPreferences("LSLogin",Context.MODE_PRIVATE);
+		
 		Button btnLogin = (Button)findViewById(R.id.btnLogin);
+		EditText edtLogin = (EditText)findViewById(R.id.edtLogin);
+		EditText edtPassword = (EditText)findViewById(R.id.edtPassword);
+		
+		edtLogin.setText(prefs.getString("user", ""));
+		edtPassword.setText(prefs.getString("pass", ""));
+		
 		btnLogin.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
+				
 				EditText edtLogin = (EditText)findViewById(R.id.edtLogin);
 				EditText edtPassword = (EditText)findViewById(R.id.edtPassword);
 				strUser = edtLogin.getText().toString();
@@ -40,6 +52,11 @@ public class LSLoginActivity extends Activity {
 					bundle.putString("USER", strUser);
 					bundle.putString("PASS", strPass);
 					intent.putExtras(bundle);
+					
+					SharedPreferences.Editor editor = prefs.edit();
+					editor.putString("user", strUser);
+					editor.putString("pass", strPass);
+					editor.commit();
 					
 					startActivity(intent);
 				}
