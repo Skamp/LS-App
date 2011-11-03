c3e38e7f365a53515df6561c3b455381f91b7bc7
Skamp <sergio.gd@gmail.com>
2011-11-03 16:53:35 +0100

    QR Code reader

--- src/com/lsn/LoadSensing/LSQRCodeActivity.java
+++ src/com/lsn/LoadSensing/LSQRCodeActivity.java
@@ -1,14 +1,46 @@
 package com.lsn.LoadSensing;
 
 import greendroid.app.GDActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class LSQRCodeActivity extends GDActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setActionBarContentView(R.layout.main);
+        setActionBarContentView(R.layout.act_03_qrcode);
         
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+		startActivityForResult(intent,0);
     }
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+		if (requestCode==0)
+		{
+			if (resultCode == RESULT_OK)
+			{
+				//Handle successful scan
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				TextView txtResult = (TextView) findViewById(R.id.txtResultValue);
+				TextView txtResultFormat = (TextView) findViewById(R.id.txtResultFormat);
+				txtResult.setText(contents);
+				txtResultFormat.setText(format);
+			}
+			else if (resultCode == RESULT_CANCELED)
+			{
+				//Handle cancel
+			}
+		}
+	}
+	
+	
+	
+	
 }
